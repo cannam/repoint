99fc450dbd253b8b9814a0483b851a4c1208b30a
Chris Cannam <cannam@all-day-breakfast.com>
2017-06-05 15:17:15 +0100

    Two dynamic tests

---
+++ test/tests/test-unpin-and-update.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+. $(dirname "$0")/include.sh
+
+libcontent_pinned=$(cat <<EOF
+"A": {
+    "vcs": "hg",
+    "service": "testfile",
+    "pin": "1379d75"
+},
+"B": {
+    "vcs": "git",
+    "service": "testfile",
+    "pin": "7219cf6e6"
+}
+EOF
+          )
+
+libcontent_unpinned=$(cat <<EOF
+"A": {
+    "vcs": "hg",
+    "service": "testfile"
+},
+"B": {
+    "vcs": "git",
+    "service": "testfile"
+}
+EOF
+          )
+
+prepare
+write_project_file "$libcontent_pinned"
+"$vextdir"/vext install
+check_expected 1379d75f0b4f 7219cf6e6d4706295246d278a3821ea923e1dfe2
+
+write_project_file "$libcontent_unpinned"
+"$vextdir"/vext install # obeys lock file, so should do nothing
+check_expected 1379d75f0b4f 7219cf6e6d4706295246d278a3821ea923e1dfe2
+
+"$vextdir"/vext update
+check_expected f94ae9d7e5c9 3199655c658ff337ce24f78c6d1f410f34f4c6f2
+
