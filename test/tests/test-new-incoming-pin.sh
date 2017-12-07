391fe017d791bed237da8dcb48f2b4ca3f38aade
Chris Cannam <cannam@all-day-breakfast.com>
2017-12-07 18:40:12 +0000

    Update two more tests

--- test/tests/test-new-incoming-pin.sh
+++ test/tests/test-new-incoming-pin.sh
@@ -27,12 +27,12 @@ libcontent=$(cat <<EOF
 EOF
           )
 
-##!!! todo: finish this for svn
-
 ( cd ../../testrepos
-  rm -rf A2 B2
+  rm -rf A2 B2 C2 C2_checkout
   hg clone A A2
   git clone -bmaster B B2
+  cp -a C C2
+  svn co file://$(pwd)/C2 C2_checkout
 )
 
 prepare
@@ -48,10 +48,15 @@ check_expected f94ae9d7e5c9 3199655c658ff337ce24f78c6d1f410f34f4c6f2 2
   cd ../B2
   echo 5 > file-b.txt
   git commit -a -m 5
+  cd ../C2_checkout
+  echo 5 > file.txt
+  svn commit -m 5
+  svn update
 )
 
 newidA=$( cd ../../testrepos/A2 ; hg id | awk '{ print $1; }' )
 newidB=$( cd ../../testrepos/B2 ; git rev-parse HEAD )
+newidC=$( cd ../../testrepos/C2_checkout ; svn info --show-item revision )
 
 libcontent_pinned=$(cat <<EOF
 "A": {
--- test/tests/test-new-incoming-unpinned.sh
+++ test/tests/test-new-incoming-unpinned.sh
@@ -18,21 +18,28 @@ libcontent=$(cat <<EOF
     "vcs": "git",
     "service": "testfile",
     "repository": "B2"
+},
+"C": {
+    "vcs": "svn",
+    "service": "testfile",
+    "repository": "C2"
 }
 EOF
           )
 
 ( cd ../../testrepos
-  rm -rf A2 B2
+  rm -rf A2 B2 C2 C2_checkout
   hg clone A A2
   git clone -bmaster B B2
+  cp -a C C2
+  svn co file://$(pwd)/C2 C2_checkout
 )
 
 prepare
 write_project_file "$libcontent"
 
 "$vext" install
-check_expected f94ae9d7e5c9 3199655c658ff337ce24f78c6d1f410f34f4c6f2
+check_expected f94ae9d7e5c9 3199655c658ff337ce24f78c6d1f410f34f4c6f2 2
 
 ( cd ../../testrepos
   cd A2
@@ -41,14 +48,19 @@ check_expected f94ae9d7e5c9 3199655c658ff337ce24f78c6d1f410f34f4c6f2
   cd ../B2
   echo 5 > file-b.txt
   git commit -a -m 5
+  cd ../C2_checkout
+  echo 5 > file.txt
+  svn commit -m 5
+  svn update
 )
 
 newidA=$( cd ../../testrepos/A2 ; hg id | awk '{ print $1; }' )
 newidB=$( cd ../../testrepos/B2 ; git rev-parse HEAD )
+newidC=$( cd ../../testrepos/C2_checkout ; svn info --show-item revision )
 
 "$vext" install
-check_expected f94ae9d7e5c9 3199655c658ff337ce24f78c6d1f410f34f4c6f2
+check_expected f94ae9d7e5c9 3199655c658ff337ce24f78c6d1f410f34f4c6f2 2
 
 "$vext" update
-check_expected $newidA $newidB
+check_expected $newidA $newidB $newidC
 
